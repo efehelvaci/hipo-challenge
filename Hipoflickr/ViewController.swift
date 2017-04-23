863ce8116b734ab313819af56592a79f6a7c9827
Efe Helvacı <efe.459@gmail.com>
2017-04-23 22:45:48 +0300

    Pods initialized

---
+++ Hipoflickr/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hipoflickr
+//
+//  Created by Efe Helvacı on 23.04.2017.
+//  Copyright © 2017 Efe Helvacı. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,12 @@
+use_frameworks!
+
+target 'Hipoflickr' do
+
+	pod 'Alamofire', '~> 4.4'
+	pod 'AsyncSwift', '~> 2.0.0'
+	pod 'FTIndicator', '~> 1.2'
+	pod 'SwifterSwift' , :git => 'https://github.com/omaralbeik/SwifterSwift.git'
+	pod 'SwiftyJSON', '~> 3.1'
+	pod 'Kingfisher', '~> 3.6'
+
+end
